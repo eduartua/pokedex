d38d622502d770ec9b851624167b07099e3f3084
eduartua <eduartua@gmail.com>
2024-04-26 20:57:40 -0600

    Add Theme Context

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,12 @@
-import { Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import { View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
 
 export const HomeScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Text
+        variant='headlineLarge'
+      >HomeScreen</Text>
 
       <Button 
         mode="contained" 
