aa60266408e2a5a3ababec0e2afcc1c55dc1c3cd
eduartua <eduartua@gmail.com>
2024-05-13 16:28:44 -0600

    Show favorites on screen

--- src/presentation/screens/search/SearchScreen.tsx
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -4,10 +4,48 @@ import { globalTheme } from '../../../config/theme/global-theme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { PokemonCard } from '../../components/pokemons/PokemonCard';
 import { Pokemon } from '../../../domain/entities/pokemon';
+import { useQuery } from '@tanstack/react-query';
+import { getPokemonNamesWithId, getPokemonsByIds } from '../../../actions/pokemons';
+import { useMemo, useState } from 'react';
+import { FullScreenLoader } from '../../components/ui/FullScreenLoader';
 
 export const SearchScreen = () => {
 
   const { top } = useSafeAreaInsets();
+
+  const [term, setTerm] = useState('')
+
+  const { isLoading, data: pokemonNameList = [] } = useQuery({
+    queryKey: ['pokemons', 'all'],
+    queryFn: () => getPokemonNamesWithId()
+  });
+
+  // TODO: apply debouncer
+  const pokemonNameIdList = useMemo(() => {
+    // Is a number
+    if ( !isNaN(Number(term))) {
+      const pokemon = pokemonNameList.find( pokemon => pokemon.id === Number(term) );
+      return pokemon ? [pokemon]: [];
+    }
+
+    if ( term.length === 0 ) return [];
+    if ( term.length < 3 ) return [];
+
+    return pokemonNameList.filter(pokemon => 
+      pokemon.name.includes(term.toLocaleLowerCase()),
+    );
+  }, [term])
+
+  const { isLoading: isLoadingPokemons, data: pokemons = [] } = useQuery({
+    queryKey: ['pokemons', 'by', pokemonNameIdList],
+    queryFn: () => getPokemonsByIds(pokemonNameIdList.map(pokemon => pokemon.id )),
+    staleTime: 1000 * 60 * 5,
+  });
+
+  if (isLoading) {
+    return (<FullScreenLoader />);
+  }
+  
 
   return (
     <View style={[ globalTheme.globalMargin, { paddingTop: top + 10 } ]}>
@@ -16,18 +54,20 @@ export const SearchScreen = () => {
         mode='flat'
         autoFocus
         autoCorrect={ false }
-        onChangeText={ value => console.log(value) }
-        value={ '' }
+        onChangeText={ setTerm }
+        value={ term }
       />
-      <ActivityIndicator style={{ paddingTop: 20 }} />
+      
+      {isLoadingPokemons && <ActivityIndicator style={{ paddingTop: 20 }} /> }
 
       <FlatList
-        data={[] as Pokemon[]}
+        data={ pokemons }
         keyExtractor={ (pokemon, index) => `${pokemon.id}-${ index }` }
         numColumns={2}
         style={{paddingTop: top + 20,}}
         renderItem={ ({item}) => <PokemonCard pokemon={item}/>}
         showsVerticalScrollIndicator={ false }
+        ListFooterComponent={<View style={{ height: 100 }} />}
       />
     </View>
   )
